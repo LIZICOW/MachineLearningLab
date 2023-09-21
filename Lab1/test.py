7cd5a646f6fd7f2db64fa6cdffedadd345ebc58a
Ritzning <1538957792@qq.com>
2023-09-21 21:21:27 +0800

    添加adam

--- Lab1/test.py
+++ Lab1/test.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 a = np.array([[1, 2 ,3], [4, 5, 6]])
-print(a.shape)
+print(np.zeros([2, 9]))
+print(a**2)
 print(np.sum(a,axis=0,keepdims=True))
 
 pre = np.array([[0.1,0.3,0.6],[0.2,0.6,0.2],[0.9,0.1,0],[0.3,0.4,0.3]])
