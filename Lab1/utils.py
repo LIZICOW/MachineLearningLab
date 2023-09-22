ea506f5a2cd76e1e8214c408822bd7c7ac3b9269
pkyprivate <122878022+pkyprivate@users.noreply.github.com>
2023-09-22 21:33:45 +0800

    Update utils.py

--- Lab1/utils.py
+++ Lab1/utils.py
@@ -1,11 +1,31 @@
 import numpy as np
-import neuralNetwork as nn
+import neuralNetwork 
+
+
+class CustomStandardScaler:
+    def __init__(self):
+        self.mean_ = None
+        self.scale_ = None
+
+    def fit(self, X):
+        self.mean_ = np.mean(X, axis=0)
+        self.scale_ = np.std(X, axis=0)
+
+    def transform(self, X):
+        if self.mean_ is None or self.scale_ is None:
+            raise ValueError("Scaler has not been fitted. Call fit() first.")
+
+        return (X - self.mean_) / self.scale_
+
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
+
+
 class dataSet:
     def __init__(self, x, y):
         assert isinstance(x, np.ndarray)
         assert isinstance(y, np.ndarray)
-        assert np.issubdtype(x.dtype, np.float32)
-        assert np.issubdtype(y.dtype, np.float32)
         assert x.ndim == 2
         assert y.ndim == 2
         assert x.shape[0] == y.shape[0]
@@ -13,13 +33,11 @@ class dataSet:
         self.y = y
 
     def iterate(self, batch_size):
-        assert isinstance(batch_size, int) and batch_size > 0, (
-            "Batch size should be a positive integer, expected int, got {!r}".format(
-                batch_size))
-        assert self.x.shape[0] % batch_size == 0, (
-            "Dataset size {:d} is not divisible by batch size {:d}".format(
-                self.x.shape[0], batch_size))
-        for i in range(0, batch_size, self.x.shape[0]):
-            x = self.x[i: i + batch_size]
-            y = self.y[i: i + batch_size]
-            yield nn.Constant(x), nn.Constant(y)
+        num_batches = self.x.shape[0] // batch_size  # 计算可完整迭代的批次数
+        for i in range(num_batches):
+            start_idx = i * batch_size
+            end_idx = (i + 1) * batch_size
+            x = self.x[start_idx:end_idx]
+            y = self.y[start_idx:end_idx]
+            yield neuralNetwork.Constant(x), neuralNetwork.Constant(y)
+
