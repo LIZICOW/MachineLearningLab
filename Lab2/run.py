61b86cc09d657a66587bb996424b502a15cc428d
ZhangZuhao <104840093+haoo-Z@users.noreply.github.com>
2023-11-17 09:22:42 +0800

    run.py
    添加可视化部分

--- Lab2/run.py
+++ Lab2/run.py
@@ -1,18 +1,27 @@
-from Xgb import XGB
 import pandas as pd
 from model import Model
+from Visualizer import DataVisualizer
 
 df = pd.read_csv('./data/StressLevelDataset.csv')
-# print(df)
+
+#可视化展现数据集特征
+DataVisualizer(df).plot_heatmap(df)
+DataVisualizer(df).plot_violin(df)
+DataVisualizer(df).plot_boxplot(df)
+
 X = df[[x for x in df.columns if x != 'stress_level']]
 Y = df['stress_level']
 
-# xgb = XGB(n_estimators=2, max_depth=2, reg_lambda=1, min_child_weight=1, objective='linear')
-# xgb.fit(X,Y)
 input_dim = len(X.columns)
 print(input_dim)
 
 model = Model(input_dim=input_dim, num_classes=3, lr=0.01)
 model.load_data(X, Y)
-model.train(epochs=1000)
-model.test()
+train_losses= model.train(epochs=1000)
+y_pred, y_true, y_scores = model.test()
+# 绘制损失函数的变化图
+DataVisualizer(model).plot_loss(train_losses)
+# 绘制混淆矩阵
+DataVisualizer(model).plot_confusion_matrix(y_true, y_pred)
+
+
